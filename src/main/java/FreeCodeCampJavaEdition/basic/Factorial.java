f749f70d7a2e892eee0196b3010b274669b88796
Mattw46 <mattw76@live.com.au>
2019-03-08 01:33:58 +0000

    moved Factorial tests to testJunit1

--- src/main/java/FreeCodeCampJavaEdition/basic/Factorial.java
+++ src/main/java/FreeCodeCampJavaEdition/basic/Factorial.java
@@ -12,20 +12,5 @@ public class Factorial {
         return factorial;
     }
     
-    /*public void testFactorialize()  throws AssertionError {
-        
-        System.out.println("Testing Factorialize");
-        // return int
-        assert Long.class.isInstance(factorialize(5));
-        //assert Float.class.isInstance(factorialize(5));
-        // 5 return 120
-        assert factorialize(5) == 120;
-        // 10 return 3628800
-        assert factorialize(10) == 3628800;
-        // 20 return 2432902008176640000
-        assert factorialize(20) == 2432902008176640000L;
-        // 0 return 1
-        assert factorialize(0) == 1;
-        System.out.println("Factorialize passed");
-    }*/
+    
 }
--- src/test/java/FreeCodeCampJavaEdition/TestJunit1.java
+++ src/test/java/FreeCodeCampJavaEdition/TestJunit1.java
@@ -6,10 +6,12 @@ import FreeCodeCampJavaEdition.basic.ReverseString;
 
 public class TestJunit1 {
     ReverseString rs;
+    Factorial ft;
    
     @Before
     public void setup() {
         rs = new ReverseString();
+        fr = new Factorial();
     }
     
     @Test
@@ -22,4 +24,27 @@ public class TestJunit1 {
         assert rs.reverseString("Greetings from Earth").equals("htraE morf sgniteerG");
 	    System.out.println("Reverse String Passed");  
     }
+    
+    @Test
+    public void testFactorialize() throws AssertionError {
+        System.out.println("Testing Factorialize");
+        // return int
+        assert Long.class.isInstance(factorialize(5));
+    }
+    
+    /*public void testFactorialize()  throws AssertionError {
+        
+        
+        
+        //assert Float.class.isInstance(factorialize(5));
+        // 5 return 120
+        assert factorialize(5) == 120;
+        // 10 return 3628800
+        assert factorialize(10) == 3628800;
+        // 20 return 2432902008176640000
+        assert factorialize(20) == 2432902008176640000L;
+        // 0 return 1
+        assert factorialize(0) == 1;
+        System.out.println("Factorialize passed");
+    }*/
 }
